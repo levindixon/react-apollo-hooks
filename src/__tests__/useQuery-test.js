27ac6aa4a831e2b380841429c4f4003e100b6adc
Daniel Trojanowski <daniel.trojanowski@gmail.com>
2018-11-30 10:55:42 +0100

    test: update  apollo and react versions used in tests

--- src/__tests__/useQuery-test.js
+++ src/__tests__/useQuery-test.js
@@ -313,3 +313,12 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
