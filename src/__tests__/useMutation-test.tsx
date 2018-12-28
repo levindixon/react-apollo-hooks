4f27810dafd0b28ba50d2810b3ff680fc46f574e
Umidbek Karimov <uma.karimov@gmail.com>
2018-12-28 20:53:15 +0500

    refactor(tests): use `toMatchInlineSnapshot` and `jest-react-profiler` (#46)

--- src/__tests__/useMutation-test.tsx
+++ src/__tests__/useMutation-test.tsx
@@ -252,14 +252,3 @@
 ***REMOVED***
 ***REMOVED***
 ***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
